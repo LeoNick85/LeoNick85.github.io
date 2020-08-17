2076a1dffd5e95662d602a44c6b7b6f509e4b294
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-08-17 16:04:21 +0200

    Version 1.0

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
+    //Menu a comparsa per mobile
     $(".fa-bars").click(function(){
         $("#hamburger-menu").toggle();
-    })
+    });
+
+    //Apertura dettagli su schede progetto
+    $(".card-bottom .project-details").click(function() {
+        var scheda_corrente = $(this).closest(".project-item");
+        scheda_corrente.find(".card-hidden").removeClass("card-hidden").addClass("card-back");
+        scheda_corrente.find(".card-front").removeClass("card-front").addClass("card-hidden");
+    });
+
+    //Chiusura dettagli scheda progetto
+    $(document).on("click", ".exit-details", function() {
+        var scheda_corrente = $(this).closest(".project-item");
+        scheda_corrente.find(".card-hidden").removeClass("card-hidden").addClass("card-front");
+        scheda_corrente.find(".card-back").removeClass("card-back").addClass("card-hidden");
+    });
 });
